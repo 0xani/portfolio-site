2f7b0ec14b9df939497d3eba1c3e868665422f0a
Jose Torres <jtorresuci@gmail.com>
2020-08-10 23:13:17 -0700

    Added working github repo to Python project page. Simple MP3 is now live on the site.

--- src/components/Projects/lang/Python.js
+++ src/components/Projects/lang/Python.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const projects = ["Coming Soon"]
+const projects = ["Simple MP3"]
+const links = ["https://github.com/jtorresuci/simple-mp3"]
 
 function Python (){
     return(
@@ -13,8 +14,8 @@ function Python (){
                                 <img src="https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png" className="card-img-top" alt="..."/>
                                 <div className="card-body">
                                     <h5 className="card-title">{project}</h5>
-                                    <p className="card-text">Currently in development</p>
-                                    {/*<a href="#" className="btn btn-primary">Go somewhere</a>/*}*/}
+                                    <p className="card-text">Low resource application to play music. Helps conserve battery and RAM.</p>
+                                    <a href="https://github.com/jtorresuci/simple-mp3" className="btn btn-primary">Github Repo</a>
                                 </div>
                             </div>
                         )
